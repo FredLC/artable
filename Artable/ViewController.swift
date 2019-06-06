a46265e3a763b8e3f13be6b32820f4d25ff65ae7
fredlc <fredlefevre@outlook.com>
2019-06-06 13:46:06 -0400

    Initial commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Fred Lefevre on 2019-06-06.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

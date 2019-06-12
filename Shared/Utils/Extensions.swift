f9d1057bc49969b1f631c29d87bbb61146e422d2
fredlc <fredlefevre@outlook.com>
2019-06-11 20:26:21 -0400

    Created string extension to check if a string is not empty

---
+++ Shared/Utils/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  Artable
+//
+//  Created by Fred Lefevre on 2019-06-11.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var isNotEmpty: Bool {
+        return !isEmpty
+    }
+}

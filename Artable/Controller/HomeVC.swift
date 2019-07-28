fbb288cb0c1ee008e584c5102f1143d4e9a31e7e
fredlc <fredlefevre@outlook.com>
2019-07-28 18:26:34 -0400

    Cleaned up HomeVC

--- Artable/Controller/HomeVC.swift
+++ Artable/Controller/HomeVC.swift
@@ -23,10 +23,23 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         db = Firestore.firestore()
-        
+        setupCollectionView()
+        setupInitialAnonymousUser()
+        setupNavigationBar()
+    }
+    
+    func setupNavigationBar() {
+        guard let font = UIFont(name: "futura", size: 26) else { return }
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white, NSAttributedString.Key.font: font]
+    }
+    
+    func setupCollectionView() {
         collectionView.delegate = self
         collectionView.dataSource = self
         collectionView.register(UINib(nibName: Identifiers.CategoryCell, bundle: nil), forCellWithReuseIdentifier: Identifiers.CategoryCell)
+    }
+    
+    func setupInitialAnonymousUser() {
         if Auth.auth().currentUser == nil {
             Auth.auth().signInAnonymously { (result, error) in
                 if let error = error {
@@ -59,7 +72,7 @@ class HomeVC: UIViewController {
     }
     
     func setCategoriesListener() {
-        listener = db.collection("categories").order(by: "timeStamp", descending: true).addSnapshotListener({ (snap, error) in
+        listener = db.categories.addSnapshotListener({ (snap, error) in
             if let error = error {
                 debugPrint(error.localizedDescription)
                 return
@@ -81,32 +94,6 @@ class HomeVC: UIViewController {
                 }
             })
         })
-    }
-    
-    func onDocumentAdded(change: DocumentChange, category: Category) {
-        let newIndex = Int(change.newIndex)
-        categories.insert(category, at: newIndex)
-        collectionView.insertItems(at: [IndexPath(row: newIndex, section: 0)])
-    }
-    
-    func onDocumentModified(change: DocumentChange, category: Category) {
-        if change.newIndex == change.oldIndex {
-            let index = Int(change.newIndex)
-            categories[index] = category
-            collectionView.reloadItems(at: [IndexPath(item: index, section: 0)])
-        } else {
-            let oldIndex = Int(change.oldIndex)
-            let newIndex = Int(change.newIndex)
-            categories.remove(at: oldIndex)
-            categories.insert(category, at: newIndex)
-            collectionView.moveItem(at: IndexPath(item: oldIndex, section: 0), to: IndexPath(item: newIndex, section: 0))
-        }
-    }
-    
-    func onDocumentRemoved(change: DocumentChange) {
-        let oldIndex = Int(change.oldIndex)
-        categories.remove(at: oldIndex)
-        collectionView.deleteItems(at: [IndexPath(item: oldIndex, section: 0)])
     }
     
 
@@ -136,6 +123,34 @@ class HomeVC: UIViewController {
 }
 
 extension HomeVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func onDocumentAdded(change: DocumentChange, category: Category) {
+        let newIndex = Int(change.newIndex)
+        categories.insert(category, at: newIndex)
+        collectionView.insertItems(at: [IndexPath(row: newIndex, section: 0)])
+    }
+    
+    func onDocumentModified(change: DocumentChange, category: Category) {
+        if change.newIndex == change.oldIndex {
+            let index = Int(change.newIndex)
+            categories[index] = category
+            collectionView.reloadItems(at: [IndexPath(item: index, section: 0)])
+        } else {
+            let oldIndex = Int(change.oldIndex)
+            let newIndex = Int(change.newIndex)
+            categories.remove(at: oldIndex)
+            categories.insert(category, at: newIndex)
+            collectionView.moveItem(at: IndexPath(item: oldIndex, section: 0), to: IndexPath(item: newIndex, section: 0))
+        }
+    }
+    
+    func onDocumentRemoved(change: DocumentChange) {
+        let oldIndex = Int(change.oldIndex)
+        categories.remove(at: oldIndex)
+        collectionView.deleteItems(at: [IndexPath(item: oldIndex, section: 0)])
+    }
+    
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return categories.count
     }
--- Shared/Utils/Firebase + Utils.swift
+++ Shared/Utils/Firebase + Utils.swift
@@ -7,6 +7,12 @@
 //
 import Firebase
 
+extension Firestore {
+    var categories: Query {
+        return collection("categories").order(by: "timeStamp", descending: true)
+    }
+}
+
 extension Auth {
     func handleFireAuthErrors(error: Error, vc: UIViewController) {
         if let errorCode = AuthErrorCode(rawValue: error._code) {

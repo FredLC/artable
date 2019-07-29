7e13ac8acb7b10dacfd95fa1d5c956dbf69bec4f
fredlc <fredlefevre@outlook.com>
2019-07-29 16:39:41 -0400

    AdminHomeVC setup

---
+++ ArtableAdmin/AdminHomeVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Fred Lefevre on 2019-06-06.
+//  Copyright © 2019 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class AdminHomeVC: HomeVC {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem?.isEnabled = false
+        
+        let addCategoryButton = UIBarButtonItem(title: "Add Category", style: .plain, target: self, action: #selector(addCategory))
+        navigationItem.rightBarButtonItem = addCategoryButton
+    }
+
+    @objc func addCategory() {
+        // segue to add category vc
+    }
+
+}
+
